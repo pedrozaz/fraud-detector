58424fa5f367a4e55608f63796301819df793b09
Pedrozaz <guilhermebarb0sa@proton.me>
2025-11-14 21:19:49 -0300

    feat: add jwtService implementation
    implements the jwtservice utility class responsible for creating,
    parsing, and validating json web tokens

---
+++ api/src/main/java/io/github/pedrozaz/api/config/JwtAuthenticationFilter.java
@@ -0,0 +1,23 @@
+package io.github.pedrozaz.api.config;
+
+import io.github.pedrozaz.api.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    }
+}
